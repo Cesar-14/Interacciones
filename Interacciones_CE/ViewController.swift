137443fce47be178c56588342e790fa9c2a647f3
Alumno <itonygame@gmail.com>
2022-08-18 07:21:19 -0700

    Initial Commit

---
+++ Interacciones_CE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interacciones_CE
+//
+//  Created by Alumno on 18/08/22.
+//  Copyright © 2022 César Escobedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
